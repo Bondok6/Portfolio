00b44a7bf9c63e92ac2766d0a4d65c32f3c55aef
Bondok6 <kirolousbondok6@gmail.com>
2021-12-01 20:24:11 +0200

    Add contact-details part in footer

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
-const form = document.getElementById('my-form')
-
-async function handleSubmit(event) {
-  let data = new FormData(event.target);
-=======
 const form = document.getElementById("my-form");
 
 async function handleSubmit(event) {
-  event.preventDefault();
-
->>>>>>> 3791068d64b26e1a2ac10ab5d5cb4b5209845aec
+  let data = new FormData(event.target);
   fetch(event.target.action, {
     method: form.method,
     body: data,
@@ -17,8 +9,6 @@ async function handleSubmit(event) {
       Accept: "application/json",
     },
   })
-<<<<<<< HEAD
-=======
     .then((response) => {
       console.log("success");
       form.reset();
@@ -26,6 +16,5 @@ async function handleSubmit(event) {
     .catch((error) => {
       console.log("error");
     });
->>>>>>> 3791068d64b26e1a2ac10ab5d5cb4b5209845aec
 }
 form.addEventListener("submit", handleSubmit);
