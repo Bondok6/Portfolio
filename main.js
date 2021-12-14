a6c7a9858ccc111308148a80cad68a62b2ee4656
Bondok6 <kirolousbondok6@gmail.com>
2021-12-14 15:38:20 +0200

    Make my design matches the original Figma design

--- main.js
+++ main.js
@@ -4,9 +4,12 @@ const mobileMenu = () => {
   const links = document.querySelectorAll('.list-item');
   const iconSrc = document.querySelector('.burger');
   const ImgUrl = document.querySelector('.burger').src;
+  const welcomeLogo = document.querySelector('.logo');
 
   const openMenu = () => {
     menuDisplay.classList.toggle('menu-active');
+    if (welcomeLogo.style.display !== 'none') welcomeLogo.style.display = 'none';
+    else welcomeLogo.style.display = 'block';
     if (iconSrc.src === ImgUrl) iconSrc.src = 'images/exit.svg';
     else iconSrc.src = ImgUrl;
   };
