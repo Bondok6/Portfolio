588cb6bd61ad1f70216f65e6b35cd88e277a4222
Bondok6 <kirolousbondok6@gmail.com>
2021-12-14 02:29:15 +0200

    Add exit icon

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
-const mobileMenu = ()=>{  
+const mobileMenu = () => {
   const btnMenu = document.querySelector(".burger");
-  const menuDisplay = document.querySelector(".nav-list")
- 
-  btnMenu.addEventListener("click",()=>{
-    menuDisplay.classList.toggle("menu-active");    
-  })
-}
+  const menuDisplay = document.querySelector(".nav-list");
+  const links = document.querySelectorAll(".list-item");
+  const iconSrc = document.querySelector(".burger");
+
+  btnMenu.addEventListener("click", () => {
+    menuDisplay.classList.toggle("menu-active");
+    iconSrc.src = "images/exit.svg";
+  });
+
+  links.forEach((link) => {
+    link.addEventListener("click", () => {
+      menuDisplay.classList.remove("menu-active");
+    });
+  });
+
+  // btnMenu.addEventListener("keydown", (e) => {
+  //   if (e.key === "") {
+  //     menuDisplay.classList.toggle("menu-active");
+  //     iconSrc.src = "images/exit.svg";
+  //   }
+  // });
+};
 
 mobileMenu();
